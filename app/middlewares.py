1d3bcacc64ea9b3aac2ecefb1b60738871cc1785
kinder <141255660+anuware@users.noreply.github.com>
2024-08-08 02:30:41 +0300

    inital commit

---
+++ app/middlewares.py
@@ -0,0 +1,8 @@
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+
+from typing import Callable, Dict, Any, Awaitable
+
+
+class BaseMiddleware(BaseMiddleware):
+    pass #middleware here
---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN=''
+DB_URL='sqlite+aiosqlite:///db.sqlite3'
+
+#WARNING: add config.py in gitignore
